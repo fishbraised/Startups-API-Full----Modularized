2ac41101076d644cf0307e7e5f2584dd73a2498e
fishbraised <fishbraised@gmail.com>
2026-09-09 19:46:58 +0800

    Minor changes.

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ app.use((req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at port:${port}...`);
 });
+
+// after doing everything commit and check if redeployed in Render or not.
+
+// use react-router (frontend):
+// after doing all that, test in a small new react project with a basic startup card for each startup
+// to make a fetch call to this Api.
+// you can create a router for the small, new react project where it displays startup card in startups routes and user cards in userroutes.
+
+// ^^ Summary: there is your own express server, and you use the express server for react-router in a new project.
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,6 @@ import getUsers from "../controllers/getUsers.js";
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
+// Make user Search route is here.
 
 export default userRouter;
