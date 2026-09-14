c2454fe11433d849cd5ee0511d234ed9983bd87c
fishbraised <fishbraised@gmail.com>
2026-09-14 19:28:02 +0800

    CORS enabled only to Netlify project.

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://react-startup-industry.netlify.app",
+  }),
+);
 
 app.use("/startups", startUpRouter);
 app.use("/users", userRouter);
